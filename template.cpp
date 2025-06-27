6ab0918e9f95c2bc7cbb2ba3f97745cc5b5b165e
Gyan Prakash Tiwari <gyanprakash.eleven@gmail.com>
2025-06-27 08:17:25 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define lld long double
+
+
+
+
+void solve() {
+
+
+
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << setprecision(15);
+
+	int tc = 1;
+	// cin >> tc;
+
+	for (int t = 1; t <= tc; t++) {
+		// cout << "Case #" << t << ": ";
+		solve();
+	}
+	return 0;
+}
